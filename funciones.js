4303b70f0595d03f9d5ec7636ef91b131559b5b0
William <william.marengo@itservice.com.pe>
2025-03-26 14:35:19 -0500

    up

--- funciones.js
+++ funciones.js
@@ -1,12 +1,12 @@
 // script.js
 const messages = {
-    2017: "Nuestro primer año juntos, lleno de momentos inolvidables 💖.",
-    2018: "Seguimos creciendo juntos y enfrentando nuevas aventuras. ✨",
-    2019: "Un año especial donde vivimos momentos increíbles 😍.",
-    2020: "A pesar de los retos, nuestro amor se hizo más fuerte 💕.",
-    2021: "Cada día contigo es un regalo hermoso 🎁.",
-    2022: "Un año lleno de amor, risas y momentos mágicos. ✨",
-    2023: "Celebramos 7 años juntos, y cada momento ha sido único ❤️."
+    2019: "Nuestro primer año juntos, lleno de momentos inolvidables 💖.",
+    2020: "Seguimos creciendo juntos y enfrentando nuevas aventuras. ✨",
+    2021: "Un año especial donde vivimos momentos increíbles 😍.",
+    2022: "A pesar de los retos, nuestro amor se hizo más fuerte 💕.",
+    2023: "Cada día contigo es un regalo hermoso 🎁.",
+    2024: "Un año lleno de amor, risas y momentos mágicos. ✨",
+    2025: "Celebramos 7 años juntos, y cada momento ha sido único ❤️."
 };
 
 function showMessage(year) {
